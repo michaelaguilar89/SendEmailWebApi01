cbefb3a985fc2445977be41129b175304e855df1
unknown <michaelaguilar89>
2024-10-04 17:52:38 -0600

    first commit

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendEmailWebApi.Models;
+using SendEmailWebApi.Services;
+
+namespace SendEmailWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly EmailService _emailService;
+        public EmailController(EmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public async Task< IActionResult> SendEmail(EmailDto request)
+        {
+            try
+            {
+               await _emailService.SendEmail(request);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+
+            }
+        }
+
+    }
+}
